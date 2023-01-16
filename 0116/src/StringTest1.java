9a00169fff30a3d60612b71224260a4269cdfe00
hbk6939951130 <hbk6939@gmail.com>
2023-01-16 14:38:43 +0900

    study

--- 0116/src/StringTest1.java
+++ 0116/src/StringTest1.java
@@ -1,7 +1,17 @@
 
 public class StringTest1 {
 	public static void main(String[] args) {
-		String str1 = "string";
-		String str2 = new String("string");
+		String str1 = "string"; // 파이썬 처럼 작동 (Perm 영역 내 string constant pool에 저장)
+		String str2 = "string";
+		String str3 = new String("string"); // heap에 저장
+		String str4 = new String("string"); // heap에 저장
+
+		System.out.println(str1 == str2); // true
+		System.out.println(str2 == str3); // false
+		System.out.println(str1 == str3); // false
+
+		System.out.println(str3 == str4); // false
+		
+		System.out.println(str3.equals(str4)); //true 
 	}
 }
